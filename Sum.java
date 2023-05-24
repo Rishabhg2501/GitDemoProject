aa3324abcac4df6743b05b58ebf701ba4d5168ca
Rishabhg2501 <133677880+Rishabhg2501@users.noreply.github.com>
2023-05-25 01:04:17 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,14 @@
+package Git;
+
+public class Sum {
+	    public static void main(String[] args) {
+	      
+	        int a=23;
+	        int b=34;
+
+	        System.out.println(a+b);
+	    }
+	        
+	    }
+
+
